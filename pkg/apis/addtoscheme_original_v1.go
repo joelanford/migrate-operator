2eeafbc780b5a6bfa9bbc7988048e8608e20bd66
Joe Lanford <joe.lanford@gmail.com>
2019-03-22 17:12:08 -0400

    original.com/v1alpha1 Kind=App -> original.com/v1 Kind=App

--- pkg/apis/addtoscheme_original_v1.go
+++ pkg/apis/addtoscheme_original_v1.go
@@ -1,10 +1,10 @@
 package apis
 
 import (
-	"github.com/joelanford/migrate-operator/pkg/apis/original/v1alpha1"
+	"github.com/joelanford/migrate-operator/pkg/apis/original/v1"
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
-	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+	AddToSchemes = append(AddToSchemes, v1.SchemeBuilder.AddToScheme)
 }
---
+++ pkg/apis/original/v1/doc.go
@@ -0,0 +1,4 @@
+// Package v1 contains API Schema definitions for the original v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=original.com
+package v1
